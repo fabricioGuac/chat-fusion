7afc5828e3c448a2250dafafa6c14f36527ec90a
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-12-05 11:08:28 -0800

    initializes the spring project

---
+++ chatfusion/src/main/java/com/chat/fusion/chatfusion/ChatfusionApplication.java
@@ -0,0 +1,13 @@
+package com.chat.fusion.chatfusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatfusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatfusionApplication.class, args);
+	}
+
+}
---
+++ chatfusion/src/test/java/com/chat/fusion/chatfusion/ChatfusionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.fusion.chatfusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatfusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
